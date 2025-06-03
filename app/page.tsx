886566cc0e890aecfbc63ad0c169731e69d227c6
Gidal <team@gidal.app>
2025-06-03 11:50:06 +0300

    Добавен shine ефект върху логото

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,35 @@
 "use client";
-
-import { useEffect } from "react";
+import Script from "next/script";
 
 export default function Home() {
-  useEffect(() => {
-    const script1 = document.createElement("script");
-    script1.src = "https://cdn.botpress.cloud/webchat/v2.4/inject.js";
-    script1.async = true;
-    document.body.appendChild(script1);
-
-    const script2 = document.createElement("script");
-    script2.src =
-      "https://files.bpcontent.cloud/2025/06/03/07/202506030730406-PYEH4AW8.js";
-    script2.async = true;
-    document.body.appendChild(script2);
-  }, []);
-
   return (
-    <main className="flex flex-col items-center justify-start pt-3 min-h-screen bg-white text-gray-800">
-      {/* ... твоето съдържание */}
-    </main>
+    <>
+      <main className="flex flex-col items-center justify-start pt-3 min-h-screen bg-white text-gray-800">
+        {/* ... съдържанието */}
+      </main>
+
+      {/* Botpress Webchat - стабилно Next.js вграждане */}
+      <Script
+        src="https://cdn.botpress.cloud/webchat/v2.4/inject.js"
+        strategy="afterInteractive"
+      />
+      <Script
+        id="botpress-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.botpressWebChat.init({
+              composerPlaceholder: "Напиши въпрос към GidBot...",
+              botId: "gidbot-demo",
+              clientId: "gidbot-demo",
+              hostUrl: "https://cdn.botpress.cloud/webchat/v2.4",
+              messagingUrl: "https://messaging.botpress.cloud",
+              botName: "GidBot",
+              enableReset: true
+            });
+          `,
+        }}
+      />
+    </>
   );
 }
